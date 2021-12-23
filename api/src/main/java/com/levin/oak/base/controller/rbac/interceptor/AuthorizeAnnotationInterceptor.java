02276e5c7a3f4940b015fd8832217992ee25d596
llw <vince2li@gmail.com>
2021-12-23 11:27:49 +0800

    优化字段长度

--- api/src/main/java/com/levin/oak/base/controller/rbac/interceptor/AuthorizeAnnotationInterceptor.java
+++ api/src/main/java/com/levin/oak/base/controller/rbac/interceptor/AuthorizeAnnotationInterceptor.java
@@ -1,17 +1,22 @@
 package com.levin.oak.base.controller.rbac.interceptor;
 
 import com.levin.oak.base.services.rbac.RbacService;
+import org.springframework.util.Assert;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AuthorizeAnnotationInterceptor implements HandlerInterceptor {
 
-    @Resource
+    //    @Resource
     RbacService rbacService;
+
+    public AuthorizeAnnotationInterceptor(RbacService rbacService) {
+        Assert.isNull(rbacService, "rbacService is null");
+        this.rbacService = rbacService;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
